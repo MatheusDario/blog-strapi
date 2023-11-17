c47cda02829c77a36f1266b03c5151ba36959462
Matheus Dario <dario.matheus97@gmail.com>
2023-11-17 17:07:55 -0300

    add count posts

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,14 @@
 'use strict';
 
 /**
- * post controller
+ *  product controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', {
+    count(ctx) {
+        var { query } = ctx.request
+        return strapi.query('api::post.post').count({ where: query });
+    }
+});
